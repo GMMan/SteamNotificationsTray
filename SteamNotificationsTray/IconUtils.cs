df2d7fc833195f3ff577bd182205e1d789762b5b
Yukai Li <yukaili.geek@gmail.com>
2016-09-01 17:03:17 -0600

    Add icon mockups and icon helper

---
+++ SteamNotificationsTray/IconUtils.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace SteamNotificationsTray
+{
+    static class IconUtils
+    {
+        public static Icon CreateIconWithBackground(Icon origIcon, Color background, Size size)
+        {
+            using (Bitmap bmp = new Bitmap(size.Width, size.Height, PixelFormat.Format32bppArgb))
+            {
+                using (Graphics g = Graphics.FromImage(bmp))
+                {
+                    g.Clear(background);
+                    int iconXPos = (bmp.Width - origIcon.Width) / 2;
+                    int iconYPos = (bmp.Height - origIcon.Height) / 2;
+                    g.DrawIcon(origIcon, iconXPos, iconYPos);
+                    g.Flush();
+                }
+
+                IntPtr iconHandle = bmp.GetHicon();
+                Icon icon = Icon.FromHandle(iconHandle);
+                return icon;
+            }
+        }
+
+        public static Icon CreateIconWithText(string text, Font font, Color background, Size size)
+        {
+            using (Bitmap bmp = new Bitmap(size.Width, size.Height, PixelFormat.Format32bppArgb))
+            {
+                using (Graphics g = Graphics.FromImage(bmp))
+                {
+                    g.Clear(background);
+                    SizeF textSize = g.MeasureString(text, font);
+                    int textXPos = (bmp.Width - (int)Math.Ceiling(textSize.Width)) / 2;
+                    int textYPos = (bmp.Height - (int)Math.Ceiling(textSize.Height)) / 2;
+                    g.DrawString(text, font, Brushes.White, new PointF(textXPos, textYPos));
+                    g.Flush();
+                }
+
+                IntPtr iconHandle = bmp.GetHicon();
+                Icon icon = Icon.FromHandle(iconHandle);
+                return icon;
+            }
+        }
+    }
+}
--- SteamNotificationsTray/TrayAppContext.cs
+++ SteamNotificationsTray/TrayAppContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace SteamNotificationsTray
 {
@@ -15,16 +16,27 @@ namespace SteamNotificationsTray
         {
             mainIcon = new NotifyIcon
             {
-                Icon = Properties.Resources.NotificationActive,
                 ContextMenu = new ContextMenu(new MenuItem[] {
                     new MenuItem("Exit", (sender, e) =>{
                         mainIcon.Visible = false;
+                        countIcon.Visible = false;
                         Application.Exit();
                     })
-                }),
-                Visible = true
+                })
             };
-        }
 
+            mainIcon.Icon = IconUtils.CreateIconWithBackground(Properties.Resources.NotificationActive, Color.FromArgb(255, 92, 126, 16), SystemInformation.SmallIconSize);
+
+            countIcon = new NotifyIcon();
+            // 7 point for 3 digits
+            // 8 point for 2 digits
+            // 9 point for 1 digit
+            string text = "8";
+            countIcon.Icon = IconUtils.CreateIconWithText(text, new Font("Arial", 10 - text.Length, FontStyle.Regular, GraphicsUnit.Point), Color.FromArgb(255, 92, 126, 16), SystemInformation.SmallIconSize);
+
+            // Show icons, count first, main second
+            countIcon.Visible = true;
+            mainIcon.Visible = true;
+        }
     }
 }
