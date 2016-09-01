9265e38b6bb0414642863d0abd9bce81e491ee22
Yukai Li <yukaili.geek@gmail.com>
2016-09-01 15:56:33 -0600

    Create basic tray icon

--- SteamNotificationsTray/Program.cs
+++ SteamNotificationsTray/Program.cs
@@ -15,7 +15,7 @@ namespace SteamNotificationsTray
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new SettingsForm());
+            Application.Run(new TrayAppContext());
         }
     }
 }
---
+++ SteamNotificationsTray/TrayAppContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SteamNotificationsTray
+{
+    class TrayAppContext : ApplicationContext
+    {
+        NotifyIcon mainIcon;
+        NotifyIcon countIcon;
+
+        public TrayAppContext()
+        {
+            mainIcon = new NotifyIcon
+            {
+                Icon = Properties.Resources.NotificationActive,
+                ContextMenu = new ContextMenu(new MenuItem[] {
+                    new MenuItem("Exit", (sender, e) =>{
+                        mainIcon.Visible = false;
+                        Application.Exit();
+                    })
+                }),
+                Visible = true
+            };
+        }
+
+    }
+}
