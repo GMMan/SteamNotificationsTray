f278f61e101c94a44c839e7db8aab14d09ccf91c
Yukai Li <yukaili.geek@gmail.com>
2016-09-02 19:24:55 -0600

    Add notifications client

---
+++ SteamNotificationsTray/NotificationCounts.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamNotificationsTray
+{
+    class NotificationCounts
+    {
+        public int Comments { get; set; } // 4
+        public int Items { get; set; } // 5
+        public int Invites { get; set; } // 6
+        public int Gifts { get; set; } // 8
+        public int OfflineMessages { get; set; } // 9
+        public int TradeOffers { get; set; } // 1
+        public int AsyncGame { get; set; } // 2
+        public int ModeratorMessage { get; set; } // 3
+        public int HelpRequestReply { get; set; } // 10
+    }
+}
---
+++ SteamNotificationsTray/NotificationsClient.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace SteamNotificationsTray
+{
+    class NotificationsClient
+    {
+        HttpClient client;
+        HttpClientHandler handler;
+
+        public NotificationCounts PrevCounts { get; private set; }
+        public NotificationCounts CurrentCounts { get; private set; }
+
+        public NotificationsClient()
+        {
+            handler = new HttpClientHandler();
+            client = new HttpClient(handler);
+        }
+
+        public async Task<NotificationCounts> PollNotificationCounts()
+        {
+            string response = await client.GetStringAsync("https://steamcommunity.com/actions/GetNotificationCounts");
+            if (response == "null") return null;
+            NotificationCounts counts = new NotificationCounts();
+            JObject respObj = JObject.Parse(response);
+            JToken notifsObj = respObj["notifsObj"];
+            foreach (JProperty notif in notifsObj)
+            {
+                switch (notif.Name)
+                {
+                    case "4":
+                        counts.Comments = (int)notif.Value;
+                        break;
+                    case "5":
+                        counts.Items = (int)notif.Value;
+                        break;
+                    case "6":
+                        counts.Invites = (int)notif.Value;
+                        break;
+                    case "8":
+                        counts.Gifts = (int)notif.Value;
+                        break;
+                    case "9":
+                        counts.OfflineMessages = (int)notif.Value;
+                        break;
+                    case "1":
+                        counts.TradeOffers = (int)notif.Value;
+                        break;
+                    case "2":
+                        counts.AsyncGame = (int)notif.Value;
+                        break;
+                    case "3":
+                        counts.ModeratorMessage = (int)notif.Value;
+                        break;
+                    case "10":
+                        counts.HelpRequestReply = (int)notif.Value;
+                        break;
+                }
+            }
+            PrevCounts = CurrentCounts;
+            CurrentCounts = counts;
+            return counts;
+        }
+
+        public void SetCookies(CookieContainer cookies)
+        {
+            handler.CookieContainer = cookies;
+        }
+    }
+}
